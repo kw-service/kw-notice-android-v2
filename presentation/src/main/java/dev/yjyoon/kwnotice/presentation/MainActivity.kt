cf770fb83780900a54d78e22881290217e5e2bd5
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-01 22:31:06 +0900

    presentation 모듈 생성

--- presentation/src/main/java/dev/yjyoon/kwnotice/presentation/MainActivity.kt
+++ presentation/src/main/java/dev/yjyoon/kwnotice/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.yjyoon.kwnotice
+package dev.yjyoon.kwnotice.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.yjyoon.kwnotice.ui.theme.KWNoticeTheme
+import dev.yjyoon.kwnotice.presentation.ui.theme.KWNoticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
