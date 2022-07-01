12255cfd21d88d792c5eddfd39e9a72ddf6b57de
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-01 22:34:50 +0900

    모듈 include 함수 작성

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,19 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "KW Notice"
-include(":app")
-include(":domain")
-include(":data")
-include(":presentation")
+
+includeAll(
+    "app",
+    "presentation",
+    "data",
+    "domain",
+)
+
+fun includeAll(vararg names: String) {
+    names.forEach { name ->
+        val projectName = ":$name"
+        val projectPath = "modules/$name"
+        include(projectName)
+        project(projectName).projectDir = file(projectPath)
+    }
+}
