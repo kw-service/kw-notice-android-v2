5997c1d8c8f0274796baf5af2df14d0bfcaa3b0c
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-01 14:52:31 +0900

    kts로 빌드 구성 마이그레이션

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "7.3.0-beta04" apply false
+    id("com.android.library") version "7.3.0-beta04" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+}
