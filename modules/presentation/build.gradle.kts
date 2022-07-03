f05b535bfbfd5f56542bf17430350679977d2549
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-03 22:08:38 +0900

    fix: Compose 의존성을 bundles를 이용하여 일괄 추가

--- modules/presentation/build.gradle.kts
+++ modules/presentation/build.gradle.kts
@@ -37,10 +37,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.ui.core)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+
+    implementation(libs.bundles.compose)
 
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
