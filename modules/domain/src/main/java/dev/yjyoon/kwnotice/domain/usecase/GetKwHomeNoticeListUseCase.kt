e143d6f562b6a26523f57487bc2f06384ea926b5
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-13 02:56:21 +0900

    feat: 임시 공지사항 로드 유즈케이스 추가

---
+++ modules/domain/src/main/java/dev/yjyoon/kwnotice/domain/usecase/GetKwHomeNoticeListUseCase.kt
@@ -0,0 +1,11 @@
+package dev.yjyoon.kwnotice.domain.usecase
+
+import dev.yjyoon.kwnotice.domain.model.Notice
+import javax.inject.Inject
+
+class GetKwHomeNoticeListUseCase @Inject constructor() {
+
+    suspend operator fun invoke(): Result<List<Notice.KwHome>> = runCatching {
+        emptyList()
+    }
+}
