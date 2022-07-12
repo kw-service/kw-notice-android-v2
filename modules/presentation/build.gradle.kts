434f204c3952cadb8afab3d411432c9ae740f8dc
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-13 02:55:56 +0900

    add: pager 의존성 추가

--- modules/presentation/build.gradle.kts
+++ modules/presentation/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.google.accompanist.webview)
     implementation(libs.google.accompanist.systemuicontroller)
     implementation(libs.google.accompanist.navigation.animation)
+    implementation(libs.google.accompanist.pager)
 
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
