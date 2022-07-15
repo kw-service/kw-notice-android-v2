ed269e8e84208131bcadcfbdd5fffdaf489abbb1
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-16 02:04:23 +0900

    fix: 즐겨찾기 판별 방식을 combine flow stream으로 변경

--- presentation/src/main/java/dev/yjyoon/kwnotice/presentation/ui/notice/NoticeUiState.kt
+++ presentation/src/main/java/dev/yjyoon/kwnotice/presentation/ui/notice/NoticeUiState.kt
@@ -1,24 +1,26 @@
 package dev.yjyoon.kwnotice.presentation.ui.notice
 
+import dev.yjyoon.kwnotice.domain.model.Favorite
 import dev.yjyoon.kwnotice.domain.model.Notice
 
 data class NoticeUiState(
     val kwHomeNoticeUiState: KwHomeNoticeUiState,
     val swCentralNoticeUiState: SwCentralNoticeUiState,
+    val favoriteNotices: List<Favorite>
 ) {
 
     companion object {
         val Loading = NoticeUiState(
             kwHomeNoticeUiState = KwHomeNoticeUiState.Loading,
-            swCentralNoticeUiState = SwCentralNoticeUiState.Loading
+            swCentralNoticeUiState = SwCentralNoticeUiState.Loading,
+            favoriteNotices = emptyList()
         )
     }
 }
 
 sealed interface KwHomeNoticeUiState {
     data class Success(
-        val notices: List<Notice.KwHome>,
-        val favoriteIds: List<Long>
+        val notices: List<Notice.KwHome>
     ) : KwHomeNoticeUiState
 
     object Loading : KwHomeNoticeUiState
@@ -27,8 +29,7 @@ sealed interface KwHomeNoticeUiState {
 
 sealed interface SwCentralNoticeUiState {
     data class Success(
-        val notices: List<Notice.SwCentral>,
-        val favoriteIds: List<Long>
+        val notices: List<Notice.SwCentral>
     ) : SwCentralNoticeUiState
 
     object Loading : SwCentralNoticeUiState
--- presentation/src/main/java/dev/yjyoon/kwnotice/presentation/ui/notice/NoticeViewModel.kt
+++ presentation/src/main/java/dev/yjyoon/kwnotice/presentation/ui/notice/NoticeViewModel.kt
@@ -1,144 +1,69 @@
 package dev.yjyoon.kwnotice.presentation.ui.notice
 
-import android.util.Log
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.yjyoon.kwnotice.domain.model.Notice
 import dev.yjyoon.kwnotice.domain.model.toFavorite
 import dev.yjyoon.kwnotice.domain.usecase.favorite.AddFavoriteUseCase
 import dev.yjyoon.kwnotice.domain.usecase.favorite.DeleteFavoriteUseCase
-import dev.yjyoon.kwnotice.domain.usecase.favorite.GetFavoriteKwIdListUseCase
-import dev.yjyoon.kwnotice.domain.usecase.favorite.GetFavoriteSwIdListUseCase
+import dev.yjyoon.kwnotice.domain.usecase.favorite.GetAllFavoriteListUseCase
 import dev.yjyoon.kwnotice.domain.usecase.notice.GetKwHomeNoticeListUseCase
 import dev.yjyoon.kwnotice.domain.usecase.notice.GetSwCentralNoticeListUseCase
 import dev.yjyoon.kwnotice.presentation.ui.base.BaseViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class NoticeViewModel @Inject constructor(
+    getAllFavoriteListUseCase: GetAllFavoriteListUseCase,
     private val getKwHomeNoticeListUseCase: GetKwHomeNoticeListUseCase,
     private val getSwCentralNoticeListUseCase: GetSwCentralNoticeListUseCase,
-    private val getFavoriteKwIdListUseCase: GetFavoriteKwIdListUseCase,
-    private val getFavoriteSwIdListUseCase: GetFavoriteSwIdListUseCase,
     private val addFavoriteUseCase: AddFavoriteUseCase,
     private val deleteFavoriteUseCase: DeleteFavoriteUseCase
 ) : BaseViewModel() {
 
-    private val _uiState = MutableStateFlow(NoticeUiState.Loading)
-    val uiState: StateFlow<NoticeUiState> = _uiState.asStateFlow()
-
-    init {
-        fetch()
-    }
-
-    private fun fetch() {
-        fetchKwHome()
-        fetchSwCentral()
-    }
-
-    private fun fetchKwHome() {
-        launch {
+    val uiState: StateFlow<NoticeUiState> = combine(
+        flow {
             getKwHomeNoticeListUseCase()
-                .onSuccess { notices ->
-                    val favoriteIds = getFavoriteKwIdListUseCase().getOrThrow()
-                    _uiState.update {
-                        it.copy(
-                            kwHomeNoticeUiState = KwHomeNoticeUiState.Success(
-                                notices = notices,
-                                favoriteIds = favoriteIds
-                            )
-                        )
-                    }
-                }
-                .onFailure { throwable ->
-                    _uiState.update {
-                        it.copy(kwHomeNoticeUiState = KwHomeNoticeUiState.Failure)
-                    }
-                    Log.e(null, throwable.stackTraceToString())
-                }
-        }
-    }
-
-    private fun fetchSwCentral() {
-        launch {
+                .onSuccess { emit(it) }
+                .onFailure { emit(null) }
+        },
+        flow {
             getSwCentralNoticeListUseCase()
-                .onSuccess { notices ->
-                    val favoriteIds = getFavoriteSwIdListUseCase().getOrThrow()
-                    _uiState.update {
-                        it.copy(
-                            swCentralNoticeUiState = SwCentralNoticeUiState.Success(
-                                notices = notices,
-                                favoriteIds = favoriteIds
-                            )
-                        )
-                    }
-                }
-                .onFailure { throwable ->
-                    _uiState.update {
-                        it.copy(swCentralNoticeUiState = SwCentralNoticeUiState.Failure)
-                    }
-                    Log.e(null, throwable.stackTraceToString())
-                }
-        }
-    }
+                .onSuccess { emit(it) }
+                .onFailure { emit(null) }
+        },
+        getAllFavoriteListUseCase()
+    ) { kwHomeNotices, swCentralNotices, favoriteNotices ->
+        NoticeUiState(
+            kwHomeNoticeUiState = if (kwHomeNotices != null) {
+                KwHomeNoticeUiState.Success(kwHomeNotices)
+            } else {
+                KwHomeNoticeUiState.Failure
+            },
+            swCentralNoticeUiState = if (swCentralNotices != null) {
+                SwCentralNoticeUiState.Success(swCentralNotices)
+            } else {
+                SwCentralNoticeUiState.Failure
+            },
+            favoriteNotices = favoriteNotices
+        )
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, NoticeUiState.Loading)
 
     fun addFavorite(notice: Notice) {
         launch {
             addFavoriteUseCase(notice.toFavorite())
-                .onSuccess { refreshFavorite(notice) }
         }
     }
 
     fun deleteFavorite(notice: Notice) {
         launch {
             deleteFavoriteUseCase(notice.toFavorite())
-                .onSuccess { refreshFavorite(notice) }
         }
 
     }
-
-    private fun refreshFavorite(notice: Notice) {
-        when (notice) {
-            is Notice.KwHome -> launch {
-                getFavoriteKwIdListUseCase()
-                    .onSuccess { ids ->
-                        _uiState.update {
-                            it.copy(
-                                kwHomeNoticeUiState = KwHomeNoticeUiState.Success(
-                                    notices = (_uiState.value.kwHomeNoticeUiState as KwHomeNoticeUiState.Success).notices,
-                                    favoriteIds = ids
-                                )
-                            )
-                        }
-                    }
-            }
-            is Notice.SwCentral -> launch {
-                getFavoriteSwIdListUseCase()
-                    .onSuccess { ids ->
-                        _uiState.update {
-                            it.copy(
-                                swCentralNoticeUiState = SwCentralNoticeUiState.Success(
-                                    notices = (_uiState.value.swCentralNoticeUiState as SwCentralNoticeUiState.Success).notices,
-                                    favoriteIds = ids
-                                )
-                            )
-                        }
-                    }
-            }
-        }
-    }
-
-    fun refresh() {
-        if (isLoading().not()) {
-            _uiState.value = NoticeUiState.Loading
-            fetch()
-        }
-    }
-
-    fun isLoading() =
-        _uiState.value.kwHomeNoticeUiState == KwHomeNoticeUiState.Loading
-                || _uiState.value.swCentralNoticeUiState == SwCentralNoticeUiState.Loading
 }
