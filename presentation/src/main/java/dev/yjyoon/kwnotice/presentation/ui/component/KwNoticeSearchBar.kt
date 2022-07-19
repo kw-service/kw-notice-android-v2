51e2f0eefcf009db53be5f215fe0a7c8caa8d91a
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-19 18:41:37 +0900

    feat: SearchBar 공통 컴포넌트 구현

---
+++ presentation/src/main/java/dev/yjyoon/kwnotice/presentation/ui/component/KwNoticeSearchBar.kt
@@ -0,0 +1,105 @@
+package dev.yjyoon.kwnotice.presentation.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.yjyoon.kwnotice.presentation.R
+import dev.yjyoon.kwnotice.presentation.ui.theme.KwNoticeTheme
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun KwNoticeSearchBar(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit,
+    onClose: () -> Unit,
+) {
+    var text by remember { mutableStateOf("") }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
+
+    Box {
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = modifier
+                .padding(horizontal = 18.dp, vertical = 6.dp)
+                .focusRequester(focusRequester),
+            textStyle = MaterialTheme.typography.bodyMedium,
+            placeholder = {
+                Text(
+                    stringResource(id = R.string.searchbar_placeholder),
+                    color = Color.Black.copy(alpha = 0.15f),
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1
+                )
+            },
+            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+            trailingIcon = {
+                IconButton(onClick = {
+                    keyboardController?.hide()
+                    onClose()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = null
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearch(text)
+                    keyboardController?.hide()
+                }
+            ),
+            singleLine = true,
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Black.copy(alpha = 0.05f),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun KwNoticeSearchBarPreview() {
+    KwNoticeTheme {
+        KwNoticeSearchBar(onSearch = {}, onClose = {})
+    }
+}
