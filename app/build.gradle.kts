aaa4d9e48753fb2f1040f54f07fc055fa235510d
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-23 15:12:29 +0900

    update: versionCode 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dev.yjyoon.kwnotice"
         minSdk = 24
         targetSdk = 32
-        versionCode = 4
+        versionCode = 5
         versionName = "2.0.0"
     }
 
